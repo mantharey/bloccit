0dc2ccad25230de0dc15dd1573c031ea4b3fbb5a
Samantha <samantha.forey@me.com>
2016-11-30 17:12:30 -0500

    Assignment 24: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
    before_save { self.email = email.downcase if email.present? }
+   before_save :format_name
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
    validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -11,4 +12,15 @@ class User < ActiveRecord::Base
 
 
    has_secure_password
+
+   def format_name
+      if name
+         name_array = []
+         name.split.each do |char|
+            name_array << char.capitalize
+         end
+
+         self.name = name_array.join(" ")
+      end
+   end
 end
