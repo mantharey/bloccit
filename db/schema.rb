900df410605a55140f1a90b958587fa9151663b7
Samantha <samantha.forey@me.com>
2016-12-06 21:49:39 -0500

    Checkpoint 25: Completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130195111) do
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20161204223024) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -40,23 +32,7 @@ ActiveRecord::Schema.define(version: 20161130195111) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsoredposts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "price"
@@ -65,7 +41,7 @@ ActiveRecord::Schema.define(version: 20161130195111) do
     t.integer  "topic_id"
   end
 
-  add_index "sponsoredposts", ["topic_id"], name: "index_sponsoredposts_on_topic_id"
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
